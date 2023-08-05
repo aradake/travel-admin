10103100405e996abe53c861c6cd70ab37d80809
aradake <aniket.adake@gmail.com>
2023-08-05 13:31:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './navigation-bar/components/index';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './pages/about';
+import Events from './pages/events';
+import React, { useState } from 'react';
+import Login from './components/Login/Login';
 
 function App() {
+
+  const [token, setToken] = useState();
+
+  if(token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/events' element={<Events/>} />
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/signin" element={<Login/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
